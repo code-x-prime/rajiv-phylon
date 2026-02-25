a3d66334fbf3abb7681a95f9b3fa3cf7185cde78
riteshk-007 <codeshorts007@gmail.com>
2026-02-25 17:06:22 +0530

    feat: add Vite configuration for React, Tailwind CSS, path aliases, and server settings.

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -6,10 +6,28 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  // Add the preview configuration for production builds
+  preview: {
+    port: 4173,
+    host: "0.0.0.0",
+    allowedHosts: [
+      "admin.testingkeliye.online",
+      "www.admin.testingkeliye.online",
+    ],
+  },
+  // Add server configuration for development
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "admin.testingkeliye.online",
+      "www.admin.testingkeliye.online",
+    ],
   },
 })
